ff40282ccbc9b27b964606a446ff036727518078
Alexandre <reisemh3@gmail.com>
2021-12-15 11:13:05 +0100

    Add img + containerÃ

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,17 @@ const colors = require('tailwindcss/colors');
 console.log(colors.white);
 
 module.exports = {
-  content: ['./src/**/*.{html,ts,css,scss}'],
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{html,ts,css,scss}'],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        extend: {
+            cursor: {
+                'fancy': 'url(hand.cur), pointer'
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
