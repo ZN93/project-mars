a1a463da018546bf6bd679f3705bd6cbae23edad
E-jarod <e-jarod@ateliom.fr>
2021-12-16 08:53:26 +0100

    footer ok

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
 
+import { AppRoutes } from '@app-routes';
+import { NavbarLink } from '@shared/models/navbar-link.model';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  #links: NavbarLink[] = [
+    {
+      path: AppRoutes.home,
+      linkText: 'accueil',
+    },
+    {
+      path: AppRoutes.picturesByDayGallery,
+      linkText: 'gallery par jour',
+    },
+    {
+      path: AppRoutes.userPicturesGallery,
+      linkText: 'ma gallerie',
+    },
+    {
+      path: AppRoutes.contact,
+      linkText: 'contact',
+    },
+  ];
+
+  get links(): NavbarLink[] {
+    return this.#links;
+  }
+
+  get copyRightsDeclaration(): string {
+    return '2021 elon mars, all rights not reserved';
+  }
+}
