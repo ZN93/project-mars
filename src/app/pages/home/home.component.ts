bd576342336af41b28a2c2e104e37dd7692adc1b
reisemh3 <reisemh3@gmail.com>
2021-12-17 12:25:57 +0100

    Update Info Mars Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IMarsImagePhotoDto } from '@shared/models/mars-images-dto.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  @Input() photos: IMarsImagePhotoDto[] = [];
+  @Input() emptyMessage = "Aucune photo n'a été trouvé pour cette date.";
+
+  get isEmpty(): boolean {
+    return this.photos.length <= 0;
+  }
+
+  ngOnInit(): void {}
+}
