e80298bf918a6fb7ceeda94878d0d08bcb84146f
reisemh3 <reisemh3@gmail.com>
2021-12-17 10:24:20 +0100

    Put pic in userPic

--- src/app/pages/user-pictures-gallery/user-pictures-gallery.component.ts
+++ src/app/pages/user-pictures-gallery/user-pictures-gallery.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { IMarsImagePhotoDto } from '@shared/models/mars-images-dto.model';
+import { MarsImagesService } from '@pages/mars-images.service';
 
 @Component({
   selector: 'app-user-pictures-gallery',
   templateUrl: './user-pictures-gallery.component.html',
   styleUrls: ['./user-pictures-gallery.component.scss'],
 })
-export class UserPicturesGalleryComponent {}
+export class UserPicturesGalleryComponent {
+  readonly today = new Date();
+  selectedDate = this.today;
+  photos: IMarsImagePhotoDto[] = [];
+
+  constructor(private readonly marsImagesService: MarsImagesService) {}
+
+  ngOnInit(): void {
+    this.fetchPhotosPerPage();
+  }
+
+  fetchPhotosPerPage(page = 1): void {
+    // formatted `selectedDate` for api param
+    const earthDate = this.marsImagesService.dateToStringInApiFormat(
+      this.selectedDate,
+    );
+
+    this.marsImagesService
+      .getImages({ page, earthDate })
+      .subscribe(({ body }) => {
+        if (body) this.photos = body.photos;
+        else console.error('body null');
+      });
+  }
+}
